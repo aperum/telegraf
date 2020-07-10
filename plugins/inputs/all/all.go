8cc2103a6f460cca9cc49a4a934ae1dd7e01113b
Sascha Holzleiter <sascha@root-login.org>
2020-07-10 19:26:09 +0200

    add gpsd input plugin

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -51,6 +51,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/fluentd"
 	_ "github.com/influxdata/telegraf/plugins/inputs/github"
 	_ "github.com/influxdata/telegraf/plugins/inputs/gnmi"
+	_ "github.com/influxdata/telegraf/plugins/inputs/gpsd"
 	_ "github.com/influxdata/telegraf/plugins/inputs/graylog"
 	_ "github.com/influxdata/telegraf/plugins/inputs/haproxy"
 	_ "github.com/influxdata/telegraf/plugins/inputs/hddtemp"
